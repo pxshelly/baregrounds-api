2a87a4b8a707b262f9aafb7810ad9707392e2300
Shelly <pxshelly@gmail.com>
2019-03-16 21:30:17 -0700

    Render submit form on homepage

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Form from './Form.jsx';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <Form />
+      </div>
+    )
+  }
+}
+
+export default App;
