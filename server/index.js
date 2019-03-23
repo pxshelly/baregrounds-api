882c5079f042e652737e1b8a6f97850df9da6a96
Shelly <pxshelly@gmail.com>
2019-03-22 18:52:54 -0700

    Expose '/recyclables/:item' endpoint

--- server/index.js
+++ server/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
+const axios = require('axios');
 const path = require('path');
 let port = 3002;
+const {whichBin} = require('../controllers.js')
 
 app.use(express.static(path.join(__dirname, '../client/dist/')));
 
+app.get('/recyclables/:item', whichBin);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'))
+})
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`)
