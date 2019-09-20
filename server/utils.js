75087ce12fc4e4c131a15856622611c36db7041e
Shelly <pxshelly@gmail.com>
2019-09-20 02:07:20 -0700

    Add method to sanitize recommendations

---
+++ server/utils.js
@@ -0,0 +1,20 @@
+const sanitizeRecommendation = (rec, method) => {
+  const sanitizedRecommendation = {
+    method: method
+  };
+  const requiredFields = ['item', 'bin'];
+  if (typeof rec !== object || !rec) {
+    return null;
+  }
+  for(let i = 0; i < requiredFields.length; i++) {
+    const currentField =  requiredFields[i];
+    if(rec[currentField] !== undefined) {
+      sanitizedRecommendation[currentField] = rec[currentField]; 
+    } else {
+      return null;
+    }
+  }
+  return sanitizeRecommendation;
+}
+
+module.exports = {sanitizeRecommendation};
